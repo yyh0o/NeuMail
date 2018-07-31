0cf52ff847060b3089d3243d2fb51503b313e3fd
fcyddd <41711073+fcyddd@users.noreply.github.com>
2018-07-31 16:04:43 +0800

    客户端初始化

---
+++ 客户端初始化/client.c
@@ -0,0 +1,68 @@
+#include"client.h"
+ /*********************************************
+客户端函数第一次运行时的初始化函数，负责建立用户邮箱
+返回值：-2 创建目录失败
+        0 成功
+        -1 打开文件失败
+ **************************************************/
+int clientInitiallization() {
+    int state = 0;
+    state = mkdir("client", 0777);//建立客户端根目录
+    if (state < 0)
+        return -2;
+    int number=0;
+
+    FILE *fp;
+    fp = fopen(MAILNUMBER, "w+");//建立邮件个数文件
+     if (fp==NULL)
+         return -1;
+     fwrite(&number,sizeof(int),1,fp);
+    fclose(fp);
+
+    FILE *fp1;
+    fp1 = fopen(BLACKLIST, "w+");//建立黑名单文件
+    if (fp1==NULL)
+        return -1;
+    fclose(fp1);
+
+     FILE *fp2;
+     fp2 = fopen(BLACKLISTNUM, "w+");//建立黑名单人数文件
+     if (fp1==NULL)
+         return -1;
+     fwrite(&number,sizeof(int),1,fp2);
+     fclose(fp2);
+
+     FILE *fp3;
+     fp3 = fopen(PEIZHIWENJIAN, "w+");//建立用户个数文件
+     if (fp3==NULL)
+         return -1;
+     fclose(fp3);
+
+     FILE *fp4;
+     fp4 = fopen(CONTACTLIST, "w+");//建立联系人文件
+     if (fp4==NULL)
+         return -1;
+     fclose(fp4);
+
+     FILE *fp5;
+     fp5 = fopen(CONTACTLISTNUM, "w+");//建立联系人个数文件
+     if (fp5==NULL)
+         return -1;
+     fwrite(&number,sizeof(int),1,fp5);
+     fclose(fp5);
+     state = mkdir("client/TemporaryStorage", 0777);
+    if (state < 0)
+        return -2;
+     state = mkdir("client/mailBox", 0777);
+     if (state < 0)
+         return -2;
+     FILE *fp6;
+     fp6 = fopen("client/mailBox/list.txt", "w+");//建立邮箱列表
+     if (fp6==NULL)
+         return -1;
+     fclose(fp6);
+    return 0;
+}
+int main(){
+    clientInitiallization();
+}
---
+++ 客户端初始化/client.h
@@ -0,0 +1,18 @@
+//
+// Created by nsu on 18-7-30.
+//
+
+#ifndef UNTITLED_CLIENT_H
+#define UNTITLED_CLIENT_H
+#include<stdio.h>
+#include<string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#define MAILNUMBER "client/mailNumber.txt"
+#define BLACKLIST "client/BlackList.txt"
+#define BLACKLISTNUM "client/BlackListNum.txt"
+#define PEIZHIWENJIAN "client/configurationFiles.txt"
+#define CONTACTLIST "client/ContactList.txt"
+#define CONTACTLISTNUM  "client/ContactListNum.txt"
+int clientInitiallization();
+#endif //UNTITLED_CLIENT_H
